964cb899c5b5e318d1b926d1260b767abccb6979
ranawp <ranasaha4224#gmail.com>
2022-04-19 12:33:16 +0600

    about page edited

--- src/Page/About/About.js
+++ src/Page/About/About.js
@@ -12,7 +12,7 @@ const About = () => {
                 <div className=' col-md-6 col-12'>
                     <h3 className='second-caption'>We Would Love To
                         See You Smile</h3>
-                    <p>Distinctively re-engineer revolutionary meta-services and premium architectures. Intrinsically incubate intuitive opportunities and real-time potentialities. Globally revolutionize global sources through interoperable services.
+                    <p>Hey,My Name is Rana.Distinctively re-engineer revolutionary meta-services and premium architectures. Intrinsically incubate intuitive opportunities and real-time potentialities. Globally revolutionize global sources through interoperable services.
 
                         Appropriately communicate one-to-one technology after plug-and-play networks and worldwide potentialities.</p>
                 </div>
